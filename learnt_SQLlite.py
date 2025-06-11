4ba9372391ddc98569f411a6ec8ce0b8c0179c0c
Mart_Tilda <denis02012000@hotmail.com>
2025-06-11 17:06:40 +0300

    learnt_SQLlite.py

--- learnt_SQLlite.py
+++ learnt_SQLlite.py
@@ -15,14 +15,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-'''
-    # тут происходит подключение к базе данных с таким вот именем
-    fstBD = sqlite3.connect('fstBD.db')
-
-    # тут создается курсос для работы с базой данных
-    cursor = fstBD.cursor()
-
-    # тут создается таблица с полями users, id(целочисленное поле, первичный ключ), name(текст), age(целое число)
-    fstBD.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)')
-'''
